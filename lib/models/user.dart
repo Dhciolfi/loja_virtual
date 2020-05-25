b4dfb90d7458af1d24a8c15ba216a630b2ef2b0c
Dhciolfi <daniel.ciolfi@gmail.com>
2020-05-25 19:49:01 -0300

    Salvando os Dados do Usuario

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,11 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class User {
 
-  User({this.email, this.password, this.name});
+  User({this.email, this.password, this.name, this.id});
 
+  String id;
   String name;
   String email;
   String password;
 
   String confirmPassword;
 
+  DocumentReference get firestoreRef =>
+    Firestore.instance.document('users/$id');
+
+  Future<void> saveData() async {
+    await firestoreRef.setData(toMap());
+  }
+
+  Map<String, dynamic> toMap(){
+    return {
+      'name': name,
+      'email': email,
+    };
+  }
 }
--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -38,7 +38,9 @@ class UserManager extends ChangeNotifier {
       final AuthResult result = await auth.createUserWithEmailAndPassword(
           email: user.email, password: user.password);
 
-      this.user = result.user;
+      user.id = result.user.uid;
+
+      await user.saveData();
 
       onSuccess();
     } on PlatformException catch (e){
