20c855f0fd94bcfcc35634e5f9cfd31319c37689
Dhciolfi <daniel.ciolfi@gmail.com>
2020-05-26 17:59:18 -0300

    Buscando os Tamanhos no Firebase

---
+++ lib/models/item_size.dart
@@ -0,0 +1,19 @@
+class ItemSize {
+
+  ItemSize.fromMap(Map<String, dynamic> map){
+    name = map['name'] as String;
+    price = map['price'] as num;
+    stock = map['stock'] as int;
+  }
+
+  String name;
+  num price;
+  int stock;
+
+  bool get hasStock => stock > 0;
+
+  @override
+  String toString() {
+    return 'ItemSize{name: $name, price: $price, stock: $stock}';
+  }
+}
--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:lojavirtual/models/item_size.dart';
 
 class Product {
 
@@ -7,11 +8,16 @@ class Product {
     name = document['name'] as String;
     description = document['description'] as String;
     images = List<String>.from(document.data['images'] as List<dynamic>);
+    sizes = (document.data['sizes'] as List<dynamic> ?? []).map(
+            (s) => ItemSize.fromMap(s as Map<String, dynamic>)).toList();
+
+    print(sizes);
   }
 
   String id;
   String name;
   String description;
   List<String> images;
+  List<ItemSize> sizes;
 
 }
