6ca0d3951ebbcc08c9865ca19a6ed6c000ca7617
Dhciolfi <daniel.ciolfi@gmail.com>
2020-05-27 19:47:43 -0300

    Criando a Barra Flutuante

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
 import 'package:lojavirtual/models/page_manager.dart';
+import 'package:lojavirtual/screens/home/home_screen.dart';
 import 'package:lojavirtual/screens/login/login_screen.dart';
 import 'package:lojavirtual/screens/products/products_screen.dart';
 import 'package:provider/provider.dart';
@@ -17,12 +18,7 @@ class BaseScreen extends StatelessWidget {
         controller: pageController,
         physics: const NeverScrollableScrollPhysics(),
         children: <Widget>[
-          Scaffold(
-            drawer: CustomDrawer(),
-            appBar: AppBar(
-              title: const Text('Home'),
-            ),
-          ),
+          HomeScreen(),
           ProductsScreen(),
           Scaffold(
             drawer: CustomDrawer(),
---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/common/custom_drawer/custom_drawer.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: CustomDrawer(),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: const [
+                  Color.fromARGB(255, 211, 118, 130),
+                  Color.fromARGB(255, 253, 181, 168)
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter
+              )
+            ),
+          ),
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                snap: true,
+                floating: true,
+                elevation: 0,
+                backgroundColor: Colors.transparent,
+                flexibleSpace: const FlexibleSpaceBar(
+                  title: Text('Loja do Daniel'),
+                  centerTitle: true,
+                ),
+                actions: <Widget>[
+                  IconButton(
+                    icon: Icon(Icons.shopping_cart),
+                    color: Colors.white,
+                    onPressed: () => Navigator.of(context).pushNamed('/cart'),
+                  ),
+                ],
+              ),
+              SliverToBoxAdapter(
+                child: Container(
+                  height: 2000,
+                  width: 200,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
