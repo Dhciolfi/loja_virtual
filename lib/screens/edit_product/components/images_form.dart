e75a1394c69dab29bef3ce0da39b495ac921f933
Dhciolfi <daniel.ciolfi@gmail.com>
2020-05-30 14:20:03 -0300

    Exibindo as Imagens no Formulario

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:lojavirtual/models/product.dart';
 
@@ -12,7 +15,38 @@ class ImagesForm extends StatelessWidget {
     return FormField<List<dynamic>>(
       initialValue: product.images,
       builder: (state){
-        
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Carousel(
+            images: state.value.map((image){
+              return Stack(
+                fit: StackFit.expand,
+                children: <Widget>[
+                  if(image is String)
+                    Image.network(image, fit: BoxFit.cover,)
+                  else
+                    Image.file(image as File, fit: BoxFit.cover,),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: Icon(Icons.remove),
+                      color: Colors.red,
+                      onPressed: (){
+                        state.value.remove(image);
+                        state.didChange(state.value);
+                      },
+                    ),
+                  )
+                ],
+              );
+            }).toList(),
+            dotSize: 4,
+            dotSpacing: 15,
+            dotBgColor: Colors.transparent,
+            dotColor: Theme.of(context).primaryColor,
+            autoplay: false,
+          ),
+        );
       },
     );
   }
