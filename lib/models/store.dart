cd4ab56b6f6803402a3c2fe696411c012316d35c
Dhciolfi <daniel.ciolfi@gmail.com>
2020-06-16 20:31:29 -0300

    Criando o Objeto da Loja

---
+++ lib/models/store.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/models/address.dart';
+
+class Store {
+
+  Store.fromDocument(DocumentSnapshot doc){
+    name = doc.data['name'] as String;
+    image = doc.data['image'] as String;
+    phone = doc.data['phone'] as String;
+    address = Address.fromMap(doc.data['address'] as Map<String, dynamic>);
+
+    opening = (doc.data['opening'] as Map<String, dynamic>).map((key, value) {
+      final timesString = value as String;
+
+      if(timesString != null && timesString.isNotEmpty){
+        final splitted = timesString.split(RegExp(r"[:-]"));
+
+        return MapEntry(
+          key,
+          {
+            "from": TimeOfDay(
+              hour: int.parse(splitted[0]),
+              minute: int.parse(splitted[1])
+            ),
+            "to": TimeOfDay(
+                hour: int.parse(splitted[2]),
+                minute: int.parse(splitted[3])
+            ),
+          }
+        );
+      } else {
+        return MapEntry(key, null);
+      }
+    });
+
+    print(opening);
+  }
+
+  String name;
+  String image;
+  String phone;
+  Address address;
+  Map<String, Map> opening;
+
+}
--- lib/models/stores_manager.dart
+++ lib/models/stores_manager.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:lojavirtual/models/store.dart';
 
 class StoresManager extends ChangeNotifier {
 
@@ -7,12 +8,14 @@ class StoresManager extends ChangeNotifier {
     _loadStoreList();
   }
 
+  List<Store> stores = [];
+
   final Firestore firestore = Firestore.instance;
 
   Future<void> _loadStoreList() async {
     final snapshot = await firestore.collection('stores').getDocuments();
 
-    print(snapshot.documents.first.data);
+    stores = snapshot.documents.map((e) => Store.fromDocument(e)).toList();
 
     notifyListeners();
   }
