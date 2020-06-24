1ddc4a577ad768eec3ef72047cc5005a4b391efe
Dhciolfi <daniel.ciolfi@gmail.com>
2020-06-23 21:53:40 -0300

    Trocando o Foco (iOS)

--- lib/screens/checkout/checkout_screen.dart
+++ lib/screens/checkout/checkout_screen.dart
@@ -9,7 +9,7 @@ class CheckoutScreen extends StatelessWidget {
   
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-  
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProxyProvider<CartManager, CheckoutManager>(
@@ -23,61 +23,66 @@ class CheckoutScreen extends StatelessWidget {
           title: const Text('Pagamento'),
           centerTitle: true,
         ),
-        body: Consumer<CheckoutManager>(
-          builder: (_, checkoutManager, __){
-            if(checkoutManager.loading){
-              return Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation(Colors.white),
-                    ),
-                    const SizedBox(height: 16,),
-                    Text(
-                      'Processando seu pagamento...',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 16
+        body: GestureDetector(
+          onTap: (){
+            FocusScope.of(context).unfocus();
+          },
+          child: Consumer<CheckoutManager>(
+            builder: (_, checkoutManager, __){
+              if(checkoutManager.loading){
+                return Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation(Colors.white),
                       ),
+                      const SizedBox(height: 16,),
+                      Text(
+                        'Processando seu pagamento...',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w800,
+                          fontSize: 16
+                        ),
+                      )
+                    ],
+                  ),
+                );
+              }
+
+              return Form(
+                key: formKey,
+                child: ListView(
+                  children: <Widget>[
+                    CreditCardWidget(),
+                    PriceCard(
+                      buttonText: 'Finalizar Pedido',
+                      onPressed: (){
+                        if(formKey.currentState.validate()){
+                          print('enviar');
+                          /*checkoutManager.checkout(
+                              onStockFail: (e){
+                                Navigator.of(context).popUntil(
+                                        (route) => route.settings.name == '/cart');
+                              },
+                              onSuccess: (order){
+                                Navigator.of(context).popUntil(
+                                        (route) => route.settings.name == '/');
+                                Navigator.of(context).pushNamed(
+                                    '/confirmation',
+                                    arguments: order
+                                );
+                              }
+                          );*/
+                        }
+                      },
                     )
                   ],
                 ),
               );
-            }
-
-            return Form(
-              key: formKey,
-              child: ListView(
-                children: <Widget>[
-                  CreditCardWidget(),
-                  PriceCard(
-                    buttonText: 'Finalizar Pedido',
-                    onPressed: (){
-                      if(formKey.currentState.validate()){
-                        print('enviar');
-                        /*checkoutManager.checkout(
-                            onStockFail: (e){
-                              Navigator.of(context).popUntil(
-                                      (route) => route.settings.name == '/cart');
-                            },
-                            onSuccess: (order){
-                              Navigator.of(context).popUntil(
-                                      (route) => route.settings.name == '/');
-                              Navigator.of(context).pushNamed(
-                                  '/confirmation',
-                                  arguments: order
-                              );
-                            }
-                        );*/
-                      }
-                    },
-                  )
-                ],
-              ),
-            );
-          },
+            },
+          ),
         ),
       ),
     );
--- lib/screens/checkout/components/credit_card_widget.dart
+++ lib/screens/checkout/components/credit_card_widget.dart
@@ -1,10 +1,16 @@
 import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
 import 'package:lojavirtual/screens/checkout/components/card_back.dart';
 import 'package:lojavirtual/screens/checkout/components/card_front.dart';
 
-class CreditCardWidget extends StatelessWidget {
+class CreditCardWidget extends StatefulWidget {
 
+  @override
+  _CreditCardWidgetState createState() => _CreditCardWidgetState();
+}
+
+class _CreditCardWidgetState extends State<CreditCardWidget> {
   final GlobalKey<FlipCardState> cardKey = GlobalKey<FlipCardState>();
 
   final FocusNode numberFocus = FocusNode();
@@ -12,40 +18,72 @@ class CreditCardWidget extends StatelessWidget {
   final FocusNode nameFocus = FocusNode();
   final FocusNode cvvFocus = FocusNode();
 
+  KeyboardActionsConfig _buildConfig(BuildContext context){
+    return KeyboardActionsConfig(
+      keyboardActionsPlatform: KeyboardActionsPlatform.IOS,
+      keyboardBarColor: Colors.grey[200],
+      actions: [
+        KeyboardAction(focusNode: numberFocus, displayDoneButton: false),
+        KeyboardAction(focusNode: dateFocus, displayDoneButton: false),
+        KeyboardAction(
+          focusNode: nameFocus,
+          toolbarButtons: [
+            (_){
+              return GestureDetector(
+                onTap: (){
+                  cardKey.currentState.toggleCard();
+                  cvvFocus.requestFocus();
+                },
+                child: const Padding(
+                  padding: EdgeInsets.only(right: 8),
+                  child: Text('CONTINUAR'),
+                ),
+              );
+            }
+          ]
+        ),
+      ]
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: <Widget>[
-          FlipCard(
-            key: cardKey,
-            direction: FlipDirection.HORIZONTAL,
-            speed: 700,
-            flipOnTouch: false,
-            front: CardFront(
-              numberFocus: numberFocus,
-              dateFocus: dateFocus,
-              nameFocus: nameFocus,
-              finished: (){
+    return KeyboardActions(
+      config: _buildConfig(context),
+      autoScroll: false,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16, 16, 16, 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: <Widget>[
+            FlipCard(
+              key: cardKey,
+              direction: FlipDirection.HORIZONTAL,
+              speed: 700,
+              flipOnTouch: false,
+              front: CardFront(
+                numberFocus: numberFocus,
+                dateFocus: dateFocus,
+                nameFocus: nameFocus,
+                finished: (){
+                  cardKey.currentState.toggleCard();
+                  cvvFocus.requestFocus();
+                },
+              ),
+              back: CardBack(
+                cvvFocus: cvvFocus,
+              ),
+            ),
+            FlatButton(
+              onPressed: (){
                 cardKey.currentState.toggleCard();
-                cvvFocus.requestFocus();
               },
-            ),
-            back: CardBack(
-              cvvFocus: cvvFocus,
-            ),
-          ),
-          FlatButton(
-            onPressed: (){
-              cardKey.currentState.toggleCard();
-            },
-            textColor: Colors.white,
-            padding: EdgeInsets.zero,
-            child: const Text('Virar cartão'),
-          )
-        ],
+              textColor: Colors.white,
+              padding: EdgeInsets.zero,
+              child: const Text('Virar cartão'),
+            )
+          ],
+        ),
       ),
     );
   }
