d07a1d5d0bc31240cf53e6bad3c58b6038a59784
Dhciolfi <daniel.ciolfi@gmail.com>
2020-06-28 18:01:53 -0300

    Exibindo Notificacao de Mudança de Status

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -331,7 +331,28 @@ export const onNewOrder = functions.firestore.document("/orders/{orderId}").onCr
         'Novo Pedido',
         'Nova venda realizada. Pedido: ' + orderId
     );
+});
 
+const orderStatus = new Map([
+    [0, "Cancelado"],
+    [1, "Em Preparação"],
+    [2, "Em Transporte"],
+    [3, "Entregue"]
+])
+
+export const onOrderStatusChanged = functions.firestore.document("/orders/{orderId}").onUpdate(async (snapshot, context) => {
+    const beforeStatus = snapshot.before.data().status;
+    const afterStatus = snapshot.after.data().status;
+
+    if(beforeStatus !== afterStatus){
+        const tokensUser = await getDeviceTokens(snapshot.after.data().user)
+
+        await sendPushFCM(
+            tokensUser,
+            'Pedido: ' + context.params.orderId,
+            'Status atualizado para: ' + orderStatus.get(afterStatus),
+        )
+    }
 });
 
 async function getDeviceTokens(uid: string){
